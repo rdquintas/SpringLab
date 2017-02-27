4c1b0c04eb7313b5d9ed932a04e9b9de70cc398b
Ricardo Quintas <rqcenter-web@yahoo.com>
2017-02-27 11:52:30 +0000

    Fix package

---
+++ zrqSpringCookBook/src/main/java/zrq/com/spel/annotationbased4/app.java
@@ -0,0 +1,17 @@
+package zrq.com.spel.annotationbased4;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class app {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("zrq/com/spel/annotationbased3/Beans.xml");
+		Customer cust = (Customer) context.getBean("customerBean");
+
+		System.out.println(cust);
+		
+	}
+}
