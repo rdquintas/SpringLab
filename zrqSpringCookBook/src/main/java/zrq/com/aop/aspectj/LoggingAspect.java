5a8de7336b6c1b74063d6bfa9f90afa3c7596ab7
rdquintas <rqcenter-web@yahoo.com>
2017-02-28 09:16:58 +0000

    Adding @After to AspectJ app

--- zrqSpringCookBook/src/main/java/zrq/com/aop/aspectj/LoggingAspect.java
+++ zrqSpringCookBook/src/main/java/zrq/com/aop/aspectj/LoggingAspect.java
@@ -1,6 +1,7 @@
 package zrq.com.aop.aspectj;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -9,8 +10,14 @@ public class LoggingAspect {
 
 	@Before("execution(* zrq.com.aop.aspectj.CustomerBo.addCustomer(..))")
 	public void logBefore(JoinPoint joinPoint) {
-
 		System.out.println("logBefore() is running!");
+		System.out.println("hijacked : " + joinPoint.getSignature().getName());
+		System.out.println("******");
+	}
+
+	@After("execution(* zrq.com.aop.aspectj.CustomerBo.addCustomer(..))")
+	public void logAfter(JoinPoint joinPoint) {
+		System.out.println("logAfter() is running!");
 		System.out.println("hijacked : " + joinPoint.getSignature().getName());
 		System.out.println("******");
 	}
