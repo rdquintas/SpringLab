71c03d02197cfdd5a023771159edb7e4a13011d1
Ricardo Quintas <rqcenter-web@yahoo.com>
2017-05-05 16:28:51 +0100

    Example Spring Type Conversion System

--- zrqSpringCookBook/src/main/java/zrq/com/beans/validator2/App.java
+++ zrqSpringCookBook/src/main/java/zrq/com/beans/validator2/App.java
@@ -1,34 +1,18 @@
 package zrq.com.beans.validator2;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class App {
 
 	public static void main(String[] args) {
-		// ApplicationContext context = new
-		// ClassPathXmlApplicationContext("zrq/com/beans/validator/Beans.xml");
-		//
-		// Person per = (Person) context.getBean("person");
-		// PersonValidator perVal = (PersonValidator)
-		// context.getBean("personValidator");
-		//
-		// Errors err = new BeanPropertyBindingResult(per, "objectName");
-		//
-		// perVal.validate(per, err);
-		//
-		// List<ObjectError> allObjectErrors = err.getAllErrors();
-		// for (ObjectError objectError : allObjectErrors) {
-		// if (objectError instanceof FieldError) {
-		// FieldError fieldError = (FieldError) objectError;
-		// System.out.println("Field name is " + fieldError.getField());
-		// }
-		// System.out.println("Codes " +
-		// Arrays.asList(objectError.getCodes()).toString());
-		// System.out.println("Error Code is " + objectError.getCode());
-		// System.out.println("Default message is " +
-		// objectError.getDefaultMessage());
-		// System.out.println();
-		// }
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("zrq/com/beans/validator2/Beans.xml");
+
+		Contact chris = ctx.getBean("chris", Contact.class);
+		System.out.println("Chris info: " + chris);
+		Contact myContact = ctx.getBean("myContact", Contact.class);
+		System.out.println("My contact info: " + myContact);
 	}
 }
